7febfc06eda2cc8a7db4760931086522f462892a
ismail zine el abidine <ismailzineelabidine@ismails-MacBook-Pro.local>
2026-01-14 02:21:30 +0800

    backend

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
 require('dotenv').config();
 const { startAutoUpdateScheduler } = require('./services/autoUpdateService');
 
 const app = express();
 
 // Middleware
-app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
